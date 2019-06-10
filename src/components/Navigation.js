cdb3963f0f04f0661181e53b6d66479b519c23bf
Miikis <28997062+Miikis@users.noreply.github.com>
2019-06-10 14:39:28 -0700

    Add prettier

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react"
 
 const Navigation = () => {
-  return (
-    <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
+    return (
         <div>
-          <a href="">Home</a>
+            <div className="App">
+                <h1>React Router Mini</h1>
+                <div>
+                    <a href="">Home</a>
+                </div>
+                <div>
+                    <a href="">About</a>
+                </div>
+                <div>
+                    <a href="">Contact</a>
+                </div>
+            </div>
         </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Navigation;
+export default Navigation
